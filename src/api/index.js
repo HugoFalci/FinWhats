969e9d5d22a3c0b14c15ace4ae8f2d9d545d3446
HugoFalci <hugo.flc.ribeiro@gmail.com>
2025-01-05 00:42:02 -0300

    teste 5

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import { connect } from '../config/database.js';
 import app from '../routes/api.js';
 
 export default async function handler(req, res) {
-    await connect(); // Conecta ao banco de dados se necessário
-    app(req, res); // Delegue para o Express
+    await connect();
+    app(req, res);
 }
